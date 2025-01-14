5849ed7c0c14acb435961e9458a9a82132425bd7
Karan Bhoi <bhoikaran135@gmail.com>
2025-01-14 22:36:13 +0530

    Added Product Screen Top Part and working on Product Screen Bottom Part.

---
+++ lib/data.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+String desc =
+    "The Nike Air Max 90 SP stays true to its OG roots with the iconic Waffle sole, stitched overlays and classic TPU accents. The camo upper and infrared details nod to the 2013 Atmos release, while the mesh toe combines with Max Air cushioning to add comfort to your journey.";
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nike_responsive/data.dart';
 import 'package:nike_responsive/utils.dart';
 
 void main() => runApp(MaterialApp(
@@ -39,7 +40,7 @@ class _MyAppState extends State<MyApp> {
             onPressed: () {},
           ),
           title: Text(
-            "Air Max 2025",
+            "Nike Air Max 90 SP",
             style: TextStyle(
                 color: Colors.white,
                 fontSize: screenAwareSize(18.0, context),
@@ -59,7 +60,10 @@ class _MyAppState extends State<MyApp> {
         ),
         body: SingleChildScrollView(
           child: Column(
-            children: <Widget>[ProductScreenTopPart()],
+            children: <Widget>[
+              ProductScreenTopPart(),
+              ProductScreenBottomPart(),
+            ],
           ),
         ),
       ),
@@ -79,20 +83,197 @@ class _ProductScreenTopPartState extends State<ProductScreenTopPart> {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      height: screenAwareSize(245, context),
+      height: screenAwareSize(250, context),
       child: Stack(
-        children: [
+        children: <Widget>[
           Stack(
-            children: [
+            children: <Widget>[
               Container(
-                height: 220,
-                width: 400,
+                height: 300,
+                width: 500,
                 child: Image.asset(
-                  'assets/nike_air_max_90.png',
+                  'assets/AIR_MAX_90_SP_bg_remove.png',
                   fit: BoxFit.contain,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 250, right: 35),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: screenAwareSize(35, context),
+                    height: screenAwareSize(35, context),
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        "assets/360.png",
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ),
               )
             ],
+          ),
+          Positioned(
+            left: screenAwareSize(18.0, context),
+            bottom: screenAwareSize(30.0, context),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Rating",
+                  style: TextStyle(
+                      color: Color(0xff949598), fontFamily: "Montserrat-Bold"),
+                ),
+                SizedBox(
+                  height: screenAwareSize(8.0, context),
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Color(0xffffe600),
+                    ),
+                    SizedBox(
+                      height: screenAwareSize(5.0, context),
+                    ),
+                    Text(
+                      "4.2",
+                      style: TextStyle(color: Color(0xffffe600)),
+                    ),
+                    SizedBox(
+                      height: screenAwareSize(5.0, context),
+                    ),
+                    Text(
+                      "(3078 people)",
+                      style: TextStyle(color: Colors.white, fontSize: 14.0),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProductScreenBottomPart extends StatefulWidget {
+  const ProductScreenBottomPart({super.key});
+
+  @override
+  State<ProductScreenBottomPart> createState() =>
+      _ProductScreenBottomPartState();
+}
+
+class _ProductScreenBottomPartState extends State<ProductScreenBottomPart> {
+  bool isExpended = false;
+
+  void _expend() {
+    setState(() {
+      isExpended = !isExpended;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              left: screenAwareSize(18, context),
+            ),
+            child: Text(
+              "Product Description",
+              style: TextStyle(
+                  color: Color(0xff949598),
+                  fontSize: screenAwareSize(10, context),
+                  fontFamily: "Montserrat-SemiBold"),
+            ),
+          ),
+          SizedBox(
+            height: screenAwareSize(7.0, context),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: screenAwareSize(26, context),
+              right: screenAwareSize(18, context),
+            ),
+            child: AnimatedCrossFade(
+              firstChild: Text(
+                desc,
+                maxLines: 2,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenAwareSize(10, context),
+                    fontFamily: "Montserrat-Mediul"),
+              ),
+              secondChild: Text(
+                desc,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenAwareSize(10, context),
+                    fontFamily: "Montserrat-Mediul"),
+              ),
+              crossFadeState: isExpended
+                  ? CrossFadeState.showSecond
+                  : CrossFadeState.showFirst,
+              duration: kThemeAnimationDuration,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: screenAwareSize(26, context),
+              right: screenAwareSize(18, context),
+            ),
+            child: GestureDetector(
+              onTap: _expend,
+              child: Text(
+                isExpended ? "Less" : "more..",
+                style: TextStyle(
+                    color: Color.fromARGB(255, 156, 251, 47),
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: screenAwareSize(11, context),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                left: screenAwareSize(15, context),
+                right: screenAwareSize(75, context)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Size",
+                  style: TextStyle(
+                      color: Color(0xff949598),
+                      fontSize: screenAwareSize(10, context),
+                      fontFamily: "Montserrat-SemiBold"),
+                ),
+                Text(
+                  "Quantity",
+                  style: TextStyle(
+                      color: Color(0xff949598),
+                      fontSize: screenAwareSize(10, context),
+                      fontFamily: "Montserrat-SemiBold"),
+                ),
+              ],
+            ),
           )
         ],
       ),
